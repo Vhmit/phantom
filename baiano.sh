8a36aff12739ead108572bf6225927627a7a3297
Gustavo Mendes <gusttavo.me@outlook.com>
2024-05-24 22:33:40 -0300

    CI
    Change-Id: I0c822c12671e35e6255a7ba02563119a2aa5ec43
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- baiano.sh
+++ baiano.sh
@@ -15,6 +15,14 @@ BLD_GRN=$RST$BLD$(tput setaf 2)
 BLD_BLU=$RST$BLD$(tput setaf 4)
 BLD_CYA=$RST$BLD$(tput setaf 6)
 
+# Token
+BOT_TOKEN=""
+PD_KEY=""
+
+# Chat ID
+CHAT_ID=""
+TOPIC_ID=""
+
 # User vars
 DEVICE="$1"
 AOSP_TAG="$2"
@@ -62,19 +70,54 @@ else
     JOBS=8
 fi
 
+# Send Telegram message
+push_msg() {
+    curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d "chat_id=$CHAT_ID&parse_mode=html&text=$1" -d "reply_to_message_id=$TOPIC_ID"
+}
+
 # Builder
 building() {
+    local start_time=$(date +%s)
+    push_msg "🛠 CI | PixelOS (14)%0ADevice: $DEVICE%0ALunch flavor: $LUNCH_FLAVOR"
+
     source build/envsetup.sh
     lunch "$LUNCH_FLAVOR"
     mka bacon -j "$JOBS"
+
+    local end_time=$(date +%s)
+    build_status $start_time $end_time
+}
+
+# Build time
+count_build_time() {
+    local start_time=$1
+    local end_time=$2
+
+    local duration=$((end_time - start_time))
+    local seconds=$((duration % 60))
+    local minutes=$((duration / 60 % 60))
+    local hours=$((duration / 3600))
+
+    local time_message=""
+    [ $hours -gt 0 ] && time_message="${hours}h "
+    [ $minutes -gt 0 ] && time_message="${time_message}${minutes}min "
+    [ $seconds -gt 0 ] && time_message="${time_message}${seconds}s "
+
+    echo "$time_message"
 }
 
 # Build status
 build_status() {
+    local start_time=$1
+    local end_time=$2
+    build_time=$(count_build_time $start_time $end_time)
+
     BUILD_PACKAGE="$(find "$OUT_DIR" -name "PixelOS_$DEVICE-14.0-*.zip" -print -quit)"
 
     if [ -n "$BUILD_PACKAGE" ]; then
-        echo -e "${GRN}Package Complete: $BUILD_PACKAGE${RST}"
+        BUILD_ID=$(basename $BUILD_PACKAGE)
+        SHA256SUM_CHECK=$(basename $(sha256sum $BUILD_PACKAGE))
+        push_msg "build completed sucessfully%0ATotal time elapsed: <b>$build_time</b>"
         uploading
     else
         push_buildlog
@@ -114,8 +157,8 @@ pdrain_upload() {
     local FILE_ID
     FILE_ID=$(echo "$response" | grep -Po '(?<="id":")[^"]*')
 
-    echo -e "${LIGHT_GREEN}Done!"
-    echo -e "${LIGHT_GREEN}Download: https://pixeldrain.com/u/$FILE_ID${RST}"
+    echo -e "${LIGHT_GREEN}Done!${RST}"
+    push_msg "Uploaded to PixelDrain%0A1. $BUILD_ID | <b>SHA256: </b>$SHA256SUM_CHECK%0A2. Download: https://pixeldrain.com/u/$FILE_ID"
 }
 
 gofile_upload() {
@@ -131,10 +174,12 @@ gofile_upload() {
     if [ "$UPLOAD_STATUS" = 'ok' ]; then
     local URL_ID
     URL_ID=$(echo "$response" | grep -Po '(?<="downloadPage":")[^"]*')
+    GOMD5=$(echo "$response" | grep -Po '(?<="md5":")[^"]*')
     echo -e "${ORANGE}Done!${RST}"
-    echo -e "${ORANGE}Download: $URL_ID${RST}"
+    push_msg "Uploaded to Gofile%0A1. $BUILD_ID | <b>SHA256: </b>$SHA256SUM_CHECK</b>%0A2. Download: $URL_ID"
     else
     echo -e "${RED}Upload failed!${RST}"
+    push_msg "Upload failed!"
     fi
 }
 
@@ -143,9 +188,7 @@ push_buildlog() {
     response=$(curl -s -d "$BUILD_LOG" https://paste.crdroid.net/documents)
     KEY=$(echo "$response" | grep -Po '(?<="key":")[^"]*')
 
-    echo -e "${BLD_RED}Build failed!${RST}"
-    echo -e "${BLD_RED}Log info: https://paste.crdroid.net/$KEY${RST}"
+    push_msg "Build failed!%0ALog info: https://paste.crdroid.net/$KEY"
 }
 
 building
-build_status
