cf919142947a00d69a4cb4fd2abee075ff977c0c
Gustavo Mendes <gusttavo.me@outlook.com>
2024-05-27 19:52:01 -0300

    baiano: Update
    Change-Id: I8154124946717fe29b5cba0cc3b1ab1d5059c1f5

--- baiano.sh
+++ baiano.sh
@@ -15,14 +15,6 @@ BLD_GRN=$RST$BLD$(tput setaf 2)
 BLD_BLU=$RST$BLD$(tput setaf 4)
 BLD_CYA=$RST$BLD$(tput setaf 6)
 
-# Token
-BOT_TOKEN=""
-PD_KEY=""
-
-# Chat ID
-CHAT_ID=""
-TOPIC_ID=""
-
 # User vars
 DEVICE="$1"
 AOSP_TAG="$2"
@@ -32,6 +24,14 @@ LUNCH_FLAVOR="aosp_$DEVICE-$AOSP_TAG-$BUILD_TYPE"
 # ROM vars
 ROM_DIR="$(pwd)"
 OUT_DIR="$ROM_DIR/out/target/product/$DEVICE"
+
+# CI
+if [ -e $ROM_DIR/ids.txt ]; then
+    FLAG_CI_BUILD=y
+    CHAT_ID=$(awk "NR==1{print;exit}" ids.txt)
+    BOT_TOKEN=$(awk "NR==2{print;exit}" ids.txt)
+    TOPIC_ID=$(awk "NR==3{print;exit}" ids.txt)
+fi
 
 for arg in "$@"; do
     case "$arg" in
@@ -61,31 +61,40 @@ for arg in "$@"; do
     esac
 done
 
+# Jobs
 if [ "${FLAG_FULL_JOBS}" = 'y' ]; then
     JOBS="${ALL_PROCS}"
 elif [ "${FLAG_CUSTOM_JOBS}" = 'y' ]; then
     JOBS="${CUSTOM_JOBS}"
 else
-    echo -e "${BLD_BLU}WARNING: No defined number of jobs! The default value is 8!!${RST}"
-    JOBS=8
+    echo -e "${BLD_BLU}WARNING: No defined number of jobs! The default value is 16!!${RST}"
+    JOBS=16
 fi
 
 # Send Telegram message
 push_msg() {
-    curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d "chat_id=$CHAT_ID&parse_mode=html&text=$1" -d "reply_to_message_id=$TOPIC_ID"
+    curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
+         -d "chat_id=$CHAT_ID&parse_mode=html&text=$1" \
+         -d "reply_to_message_id=$TOPIC_ID"
 }
 
 # Builder
 building() {
-    local start_time=$(date +%s)
-    push_msg "🛠 CI | PixelOS (14)%0ADevice: $DEVICE%0ALunch flavor: $LUNCH_FLAVOR"
+    if [ "${FLAG_CI_BUILD}" = 'y' ]; then
+        local start_time=$(date +%s)
+        push_msg "🛠 CI | PixelOS (14)%0ADevice: $DEVICE%0ALunch flavor: $LUNCH_FLAVOR"
+    fi
 
     source build/envsetup.sh
     lunch "$LUNCH_FLAVOR"
     mka bacon -j "$JOBS"
 
-    local end_time=$(date +%s)
-    build_status $start_time $end_time
+    if [ "${FLAG_CI_BUILD}" = 'y' ]; then
+        local end_time=$(date +%s)
+        build_status $start_time $end_time
+    else
+        build_status
+    fi
 }
 
 # Build time
@@ -108,49 +117,47 @@ count_build_time() {
 
 # Build status
 build_status() {
-    local start_time=$1
-    local end_time=$2
-    build_time=$(count_build_time $start_time $end_time)
+    if [ "${FLAG_CI_BUILD}" = 'y' ]; then
+        local start_time=$1
+        local end_time=$2
+        build_time=$(count_build_time $start_time $end_time)
+    fi
 
     BUILD_PACKAGE="$(find "$OUT_DIR" -name "PixelOS_$DEVICE-14.0-*.zip" -print -quit)"
 
     if [ -n "$BUILD_PACKAGE" ]; then
-        BUILD_ID=$(basename $BUILD_PACKAGE)
-        SHA256SUM_CHECK=$(basename $(sha256sum $BUILD_PACKAGE))
-        push_msg "build completed sucessfully%0ATotal time elapsed: <b>$build_time</b>"
+        if [ "${FLAG_CI_BUILD}" = 'y' ]; then
+            BUILD_ID=$(basename $BUILD_PACKAGE)
+            SHA256SUM_CHECK=$(sha256sum $BUILD_PACKAGE | awk '{print $1}')
+            push_msg "Build completed successfully%0ATotal time elapsed: <b>$build_time</b>"
+        fi
         uploading
     else
-        push_buildlog
+        push_log
     fi
 }
 
 uploading() {
-    # Upload 
-	case $UPLOAD_HOST in
-
-		# Gofile
-		gofile)
-			echo -e "${ORANGE}Starting upload to Gofile..."
-			gofile_upload $BUILD_PACKAGE
-			;;
-
-		# PixelDrain
-		pixeldrain)
-			echo -e "${LIGHT_GREEN}Starting upload to PixelDrain..."
-			pdrain_upload $BUILD_PACKAGE
-			;;
-
-		*)
-			echo -e "${BLD_BLU}Upload host not defined!${RST}"
-			;;
-	esac
+    case $UPLOAD_HOST in
+        gofile)
+            echo -e "${ORANGE}Starting upload to Gofile...${RST}"
+            gofile_upload $BUILD_PACKAGE
+            ;;
+        pixeldrain)
+            echo -e "${LIGHT_GREEN}Starting upload to PixelDrain...${RST}"
+            pdrain_upload $BUILD_PACKAGE
+            ;;
+        *)
+            echo -e "${BLD_BLU}Upload host not defined!${RST}"
+            ;;
+    esac
 }
 
 # Upload ROM package
 pdrain_upload() {
     local FILE_PATH="$1"
     local FILE_NAME="${FILE_PATH##*/}"
-    
+
     local response
     response=$(curl -# -F "name=$FILE_NAME" -F "file=@$FILE_PATH" "https://pixeldrain.com/api/file")
 
@@ -158,13 +165,17 @@ pdrain_upload() {
     FILE_ID=$(echo "$response" | grep -Po '(?<="id":")[^"]*')
 
     echo -e "${LIGHT_GREEN}Done!${RST}"
-    push_msg "Uploaded to PixelDrain%0A1. $BUILD_ID | <b>SHA256: </b>$SHA256SUM_CHECK%0A2. Download: https://pixeldrain.com/u/$FILE_ID"
+    if [ "${FLAG_CI_BUILD}" = 'y' ]; then
+        push_msg "Uploaded to PixelDrain%0A1. $BUILD_ID | <b>SHA256: </b>$SHA256SUM_CHECK%0A2. Download: https://pixeldrain.com/u/$FILE_ID"
+    else
+        echo -e "${LIGHT_GREEN}Download: https://pixeldrain.com/u/$FILE_ID${RST}"
+    fi
 }
 
 gofile_upload() {
     local FILE_PATH="$1"
     local FILE_NAME="${FILE_PATH##*/}"
-    
+
     local response
     response=$(curl -# -F "name=$FILE_NAME" -F "file=@$FILE_PATH" "https://store1.gofile.io/contents/uploadfile")
 
@@ -172,23 +183,36 @@ gofile_upload() {
     UPLOAD_STATUS=$(echo "$response" | grep -Po '(?<="status":")[^"]*')
 
     if [ "$UPLOAD_STATUS" = 'ok' ]; then
-    local URL_ID
-    URL_ID=$(echo "$response" | grep -Po '(?<="downloadPage":")[^"]*')
-    GOMD5=$(echo "$response" | grep -Po '(?<="md5":")[^"]*')
-    echo -e "${ORANGE}Done!${RST}"
-    push_msg "Uploaded to Gofile%0A1. $BUILD_ID | <b>SHA256: </b>$SHA256SUM_CHECK</b>%0A2. Download: $URL_ID"
+        local URL_ID
+        URL_ID=$(echo "$response" | grep -Po '(?<="downloadPage":")[^"]*')
+
+        echo -e "${ORANGE}Done!${RST}"
+        if [ "${FLAG_CI_BUILD}" = 'y' ]; then
+            push_msg "Uploaded to Gofile%0A1. $BUILD_ID | <b>SHA256: </b>$SHA256SUM_CHECK%0A1. Download: $URL_ID"
+        else
+            echo -e "${ORANGE}Download: $URL_ID${RST}"
+        fi
     else
-    echo -e "${RED}Upload failed!${RST}"
-    push_msg "Upload failed!"
+        echo -e "${RED}Upload failed!${RST}"
+        if [ "${FLAG_CI_BUILD}" = 'y' ]; then
+            push_msg "Upload failed!"
+        fi
     fi
 }
 
-push_buildlog() {
-    BUILD_LOG="$(cat $ROM_DIR/out/error.log)"
-    response=$(curl -s -d "$BUILD_LOG" https://paste.crdroid.net/documents)
-    KEY=$(echo "$response" | grep -Po '(?<="key":")[^"]*')
+push_log() {
+    LOG="$ROM_DIR/out/error.log"
 
-    push_msg "Build failed!%0ALog info: https://paste.crdroid.net/$KEY"
+    echo -e "${BLD_RED}Build failed!${RST}"
+    if [ "${FLAG_CI_BUILD}" = 'y' ]; then
+        push_msg "Build failed"
+        curl -F chat_id="$CHAT_ID" -F reply_to_message_id="$TOPIC_ID" -F document=@"$LOG" "https://api.telegram.org/bot$BOT_TOKEN/sendDocument"
+    else
+        PAST_LOG="$(cat $LOG)"
+        response=$(curl -s -d "$PAST_LOG" https://paste.crdroid.net/documents)
+        KEY=$(echo "$response" | grep -Po '(?<="key":")[^"]*')
+        echo -e "${BLD_RED}Log info: https://paste.crdroid.net/$KEY${RST}"
+    fi
 }
 
 building
