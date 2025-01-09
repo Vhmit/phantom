049172cbaee2497011d391772135d574825356a4
Gustavo Mendes <gusttavo.me@outlook.com>
2025-01-08 23:10:02 -0300

    scripts: Run shfmt
    Change-Id: I9e8a851e1748c0d81049da4c78165e80866fd770
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- baiano.sh
+++ baiano.sh
@@ -23,190 +23,190 @@ OUT_DIR="$ROM_DIR/out/target/product/$DEVICE"
 
 # CI
 if [ -f "$ROM_DIR/ids.txt" ]; then
-    FLAG_CI_BUILD=y
-    CHAT_ID=$(awk 'NR==1{print}' ids.txt)
-    BOT_TOKEN=$(awk 'NR==2{print}' ids.txt)
-    TOPIC_ID=$(awk 'NR==3{print}' ids.txt)
+  FLAG_CI_BUILD=y
+  CHAT_ID=$(awk 'NR==1{print}' ids.txt)
+  BOT_TOKEN=$(awk 'NR==2{print}' ids.txt)
+  TOPIC_ID=$(awk 'NR==3{print}' ids.txt)
 fi
 
 # Arg parsing
 for arg in "$@"; do
-    case "$arg" in
-        --makeclean)
-           echo -e "${BLD_BLU}Cleaning all compiled files from previous builds...${RST}"
-            rm -rf "$ROM_DIR/out"
-            echo -e "${BLD_BLU}Done!${RST}"
-            ;;
-        --installclean)
-            FLAG_INSTALLCLEAN_BUILD=y
-            ;;
-        --full-jobs)
-            ALL_PROCS=$(nproc)
-            FLAG_FULL_JOBS=y
-            ;;
-        --j*)
-            CUSTOM_JOBS="${arg#--j}"
-            FLAG_CUSTOM_JOBS=y
-            ;;
-        --upload-gofile)
-            UPLOAD_HOST="gofile"
-            ;;
-    esac
+  case "$arg" in
+    --makeclean)
+      echo -e "${BLD_BLU}Cleaning all compiled files from previous builds...${RST}"
+      rm -rf "$ROM_DIR/out"
+      echo -e "${BLD_BLU}Done!${RST}"
+      ;;
+    --installclean)
+      FLAG_INSTALLCLEAN_BUILD=y
+      ;;
+    --full-jobs)
+      ALL_PROCS=$(nproc)
+      FLAG_FULL_JOBS=y
+      ;;
+    --j*)
+      CUSTOM_JOBS="${arg#--j}"
+      FLAG_CUSTOM_JOBS=y
+      ;;
+    --upload-gofile)
+      UPLOAD_HOST="gofile"
+      ;;
+  esac
 done
 
 # Jobs
 if [ "${FLAG_FULL_JOBS}" = 'y' ]; then
-    JOBS="$ALL_PROCS"
+  JOBS="$ALL_PROCS"
 elif [ "${FLAG_CUSTOM_JOBS}" = 'y' ]; then
-    JOBS="$CUSTOM_JOBS"
+  JOBS="$CUSTOM_JOBS"
 else
-    echo -e "${BLD_BLU}WARNING: No number of jobs defined! Using default value: 16.${RST}"
-    JOBS=16
+  echo -e "${BLD_BLU}WARNING: No number of jobs defined! Using default value: 16.${RST}"
+  JOBS=16
 fi
 
 # Send Telegram message
 push_msg() {
-    local MSG="$1"
+  local MSG="$1"
 
-    curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
-         -d "chat_id=$CHAT_ID&parse_mode=html&text=$MSG" \
-         -d "reply_to_message_id=$TOPIC_ID" \
-         -d disable_web_page_preview=true
+  curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
+    -d "chat_id=$CHAT_ID&parse_mode=html&text=$MSG" \
+    -d "reply_to_message_id=$TOPIC_ID" \
+    -d disable_web_page_preview=true
 }
 
 # Lunch time
 lunching() {
-    [ "${FLAG_CI_BUILD}" = 'y' ] && local start_time=$(date +%s)
-    rm -f lunch_log.txt
+  [ "${FLAG_CI_BUILD}" = 'y' ] && local start_time=$(date +%s)
+  rm -f lunch_log.txt
 
-    source build/envsetup.sh
-    breakfast "$DEVICE" "$BUILD_TYPE" &> lunch_log.txt
+  source build/envsetup.sh
+  breakfast "$DEVICE" "$BUILD_TYPE" &>lunch_log.txt
 
-    if grep -q "dumpvars failed with" lunch_log.txt; then
-        echo -e "${BLD_RED}Lunch failed!${RST}"
-        if [ "${FLAG_CI_BUILD}" = 'y' ]; then
-            push_msg "Lunch failed"
-            curl -F chat_id="$CHAT_ID" -F reply_to_message_id="$TOPIC_ID" -F document=@"lunch_log.txt" \
-            "https://api.telegram.org/bot$BOT_TOKEN/sendDocument"
-        fi
-    else
-        if [ "${FLAG_CI_BUILD}" = 'y' ]; then
-            BUILD_NUMBER=$(grep '^BUILD_ID=' lunch_log.txt | cut -d'=' -f2)
-            CUSTOM_ANDROID_VERSION=$(grep '^PLATFORM_VERSION=' lunch_log.txt | cut -d'=' -f2)
-            push_msg "<b>🛠 CI | PixelOS ($CUSTOM_ANDROID_VERSION)</b>%0A<b>Device:</b> <code>$DEVICE</code>%0A<b>Build type:</b> <code>$BUILD_TYPE</code>%0A<b>ID:</b> <code>$BUILD_NUMBER</code>"
-        fi
-        building
+  if grep -q "dumpvars failed with" lunch_log.txt; then
+    echo -e "${BLD_RED}Lunch failed!${RST}"
+    if [ "${FLAG_CI_BUILD}" = 'y' ]; then
+      push_msg "Lunch failed"
+      curl -F chat_id="$CHAT_ID" -F reply_to_message_id="$TOPIC_ID" -F document=@"lunch_log.txt" \
+        "https://api.telegram.org/bot$BOT_TOKEN/sendDocument"
     fi
+  else
+    if [ "${FLAG_CI_BUILD}" = 'y' ]; then
+      BUILD_NUMBER=$(grep '^BUILD_ID=' lunch_log.txt | cut -d'=' -f2)
+      CUSTOM_ANDROID_VERSION=$(grep '^PLATFORM_VERSION=' lunch_log.txt | cut -d'=' -f2)
+      push_msg "<b>🛠 CI | PixelOS ($CUSTOM_ANDROID_VERSION)</b>%0A<b>Device:</b> <code>$DEVICE</code>%0A<b>Build type:</b> <code>$BUILD_TYPE</code>%0A<b>ID:</b> <code>$BUILD_NUMBER</code>"
+    fi
+    building
+  fi
 }
 
 # Build time
 building() {
-    [ "${FLAG_INSTALLCLEAN_BUILD}" = 'y' ] && make installclean
-    mka bacon -j "$JOBS"
+  [ "${FLAG_INSTALLCLEAN_BUILD}" = 'y' ] && make installclean
+  mka bacon -j "$JOBS"
 
-    if [ "${FLAG_CI_BUILD}" = 'y' ]; then
-        local end_time=$(date +%s)
-        build_status $start_time $end_time
-    else
-        build_status
-    fi
+  if [ "${FLAG_CI_BUILD}" = 'y' ]; then
+    local end_time=$(date +%s)
+    build_status $start_time $end_time
+  else
+    build_status
+  fi
 }
 
 # Timer
 count_build_time() {
-    local start_time=$1
-    local end_time=$2
+  local start_time=$1
+  local end_time=$2
 
-    local duration=$((end_time - start_time))
-    local seconds=$((duration % 60))
-    local minutes=$((duration / 60 % 60))
-    local hours=$((duration / 3600))
+  local duration=$((end_time - start_time))
+  local seconds=$((duration % 60))
+  local minutes=$((duration / 60 % 60))
+  local hours=$((duration / 3600))
 
-    local time_message=""
-    [ $hours -gt 0 ] && time_message="${hours}h "
-    [ $minutes -gt 0 ] && time_message="${time_message}${minutes}min "
-    [ $seconds -gt 0 ] && time_message="${time_message}${seconds}s "
+  local time_message=""
+  [ $hours -gt 0 ] && time_message="${hours}h "
+  [ $minutes -gt 0 ] && time_message="${time_message}${minutes}min "
+  [ $seconds -gt 0 ] && time_message="${time_message}${seconds}s "
 
-    echo "$time_message"
+  echo "$time_message"
 }
 
 # Build status
 build_status() {
+  if [ "${FLAG_CI_BUILD}" = 'y' ]; then
+    local start_time=$1
+    local end_time=$2
+    build_time=$(count_build_time $start_time $end_time)
+  fi
+
+  BUILD_PACKAGE="$(find "$OUT_DIR" -name "PixelOS_$DEVICE-$CUSTOM_ANDROID_VERSION.0-*.zip" -print -quit)"
+
+  if [ -n "$BUILD_PACKAGE" ]; then
     if [ "${FLAG_CI_BUILD}" = 'y' ]; then
-        local start_time=$1
-        local end_time=$2
-        build_time=$(count_build_time $start_time $end_time)
+      BUILD_NAME=$(basename "$BUILD_PACKAGE")
+      MD5_CHECK=$(md5sum "$BUILD_PACKAGE" | awk '{print $1}')
+      push_msg "Build completed successfully%0ATotal time elapsed: <b>$build_time</b>"
     fi
-
-    BUILD_PACKAGE="$(find "$OUT_DIR" -name "PixelOS_$DEVICE-$CUSTOM_ANDROID_VERSION.0-*.zip" -print -quit)"
-
-    if [ -n "$BUILD_PACKAGE" ]; then
-        if [ "${FLAG_CI_BUILD}" = 'y' ]; then
-            BUILD_NAME=$(basename "$BUILD_PACKAGE")
-            MD5_CHECK=$(md5sum "$BUILD_PACKAGE" | awk '{print $1}')
-            push_msg "Build completed successfully%0ATotal time elapsed: <b>$build_time</b>"
-        fi
-        uploading
-    else
-        push_log
-    fi
+    uploading
+  else
+    push_log
+  fi
 }
 
 # File upload
 uploading() {
-    case "$UPLOAD_HOST" in
-        gofile)
-            echo -e "${ORANGE}Starting upload to Gofile...${RST}"
-            gofile_upload "$BUILD_PACKAGE"
-            ;;
-        *)
-            echo -e "${BLD_BLU}No upload host defined!${RST}"
-            ;;
-    esac
+  case "$UPLOAD_HOST" in
+    gofile)
+      echo -e "${ORANGE}Starting upload to Gofile...${RST}"
+      gofile_upload "$BUILD_PACKAGE"
+      ;;
+    *)
+      echo -e "${BLD_BLU}No upload host defined!${RST}"
+      ;;
+  esac
 }
 
 # Upload to Gofile
 gofile_upload() {
-    local FILE_PATH="$1"
-    local FILE_NAME="${FILE_PATH##*/}"
+  local FILE_PATH="$1"
+  local FILE_NAME="${FILE_PATH##*/}"
 
-    local response
-    response=$(curl -# -F "name=$FILE_NAME" -F "file=@$FILE_PATH" "https://store1.gofile.io/contents/uploadfile")
+  local response
+  response=$(curl -# -F "name=$FILE_NAME" -F "file=@$FILE_PATH" "https://store1.gofile.io/contents/uploadfile")
 
-    local UPLOAD_STATUS
-    UPLOAD_STATUS=$(echo "$response" | grep -Po '(?<="status":")[^"]*')
+  local UPLOAD_STATUS
+  UPLOAD_STATUS=$(echo "$response" | grep -Po '(?<="status":")[^"]*')
 
-    if [ "$UPLOAD_STATUS" = 'ok' ]; then
-        local URL_ID
-        URL_ID=$(echo "$response" | grep -Po '(?<="downloadPage":")[^"]*')
+  if [ "$UPLOAD_STATUS" = 'ok' ]; then
+    local URL_ID
+    URL_ID=$(echo "$response" | grep -Po '(?<="downloadPage":")[^"]*')
 
-        echo -e "${ORANGE}Upload complete!${RST}"
-        if [ "${FLAG_CI_BUILD}" = 'y' ]; then
-            push_msg "Uploaded to Gofile%0A1. $BUILD_NAME | <b>MD5: </b><code>$MD5_CHECK</code>%0A<b>Download:</b> $URL_ID"
-        else
-            echo -e "${ORANGE}Download: $URL_ID${RST}"
-        fi
+    echo -e "${ORANGE}Upload complete!${RST}"
+    if [ "${FLAG_CI_BUILD}" = 'y' ]; then
+      push_msg "Uploaded to Gofile%0A1. $BUILD_NAME | <b>MD5: </b><code>$MD5_CHECK</code>%0A<b>Download:</b> $URL_ID"
     else
-        echo -e "${RED}Upload failed!${RST}"
-        [ "${FLAG_CI_BUILD}" = 'y' ] && push_msg "Upload failed!"
+      echo -e "${ORANGE}Download: $URL_ID${RST}"
     fi
+  else
+    echo -e "${RED}Upload failed!${RST}"
+    [ "${FLAG_CI_BUILD}" = 'y' ] && push_msg "Upload failed!"
+  fi
 }
 
 # Build log
 push_log() {
-    local LOG="$ROM_DIR/out/error.log"
+  local LOG="$ROM_DIR/out/error.log"
 
-    echo -e "${BLD_RED}Build failed!${RST}"
-    if [ "${FLAG_CI_BUILD}" = 'y' ]; then
-        push_msg "Build failed"
-        curl -F chat_id="$CHAT_ID" -F reply_to_message_id="$TOPIC_ID" -F document=@"$LOG" \
-            "https://api.telegram.org/bot$BOT_TOKEN/sendDocument"
-    else
-        PAST_LOG="$(cat $LOG)"
-        response=$(curl -s -d "$PAST_LOG" https://paste.crdroid.net/documents)
-        KEY=$(echo "$response" | grep -Po '(?<="key":")[^"]*')
-        echo -e "${BLD_RED}Log info: https://paste.crdroid.net/$KEY${RST}"
-    fi
+  echo -e "${BLD_RED}Build failed!${RST}"
+  if [ "${FLAG_CI_BUILD}" = 'y' ]; then
+    push_msg "Build failed"
+    curl -F chat_id="$CHAT_ID" -F reply_to_message_id="$TOPIC_ID" -F document=@"$LOG" \
+      "https://api.telegram.org/bot$BOT_TOKEN/sendDocument"
+  else
+    PAST_LOG="$(cat $LOG)"
+    response=$(curl -s -d "$PAST_LOG" https://paste.crdroid.net/documents)
+    KEY=$(echo "$response" | grep -Po '(?<="key":")[^"]*')
+    echo -e "${BLD_RED}Log info: https://paste.crdroid.net/$KEY${RST}"
+  fi
 }
 
 lunching
--- kramel.sh
+++ kramel.sh
@@ -33,91 +33,90 @@ IMAGE=$OUTPUT_DIR/Image
 
 # Arg parsing
 for arg in "$@"; do
-    case "$arg" in
-        --neutron-clang)
-            mkdir clang && cd clang
-            bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) -S
-            cd ..
-            ;;
-        --zyc-clang)
-            mkdir clang && cd clang
-            ZYC_VERSION=18.0.0
-            wget https://raw.githubusercontent.com/ZyCromerZ/Clang/main/Clang-main-lastbuild.txt && V="$(cat Clang-main-lastbuild.txt)"
-            wget -q https://github.com/ZyCromerZ/Clang/releases/download/${ZYC_VERSION}-$V-release/Clang-${ZYC_VERSION}-$V.tar.gz && tar -xf Clang-${ZYC_VERSION}-$V.tar.gz
-            cd ..
-            ;;
-        --aosp-clang)
-            AOSP_REVISION=r536225
-            git clone --depth=1 https://gitlab.com/kei-space/clang/$AOSP_REVISION.git clang
-            ;;
-        --full-jobs)
-            ALL_PROCS=$(nproc)
-            FLAG_FULL_JOBS=y
-            ;;
-        --j*)
-            CUSTOM_JOBS="${arg#--j}"
-            FLAG_CUSTOM_JOBS=y
-            ;;
-    esac
+  case "$arg" in
+    --neutron-clang)
+      mkdir clang && cd clang
+      bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) -S
+      cd ..
+      ;;
+    --zyc-clang)
+      mkdir clang && cd clang
+      ZYC_VERSION=18.0.0
+      wget https://raw.githubusercontent.com/ZyCromerZ/Clang/main/Clang-main-lastbuild.txt && V="$(cat Clang-main-lastbuild.txt)"
+      wget -q https://github.com/ZyCromerZ/Clang/releases/download/${ZYC_VERSION}-$V-release/Clang-${ZYC_VERSION}-$V.tar.gz && tar -xf Clang-${ZYC_VERSION}-$V.tar.gz
+      cd ..
+      ;;
+    --aosp-clang)
+      AOSP_REVISION=r536225
+      git clone --depth=1 https://gitlab.com/kei-space/clang/$AOSP_REVISION.git clang
+      ;;
+    --full-jobs)
+      ALL_PROCS=$(nproc)
+      FLAG_FULL_JOBS=y
+      ;;
+    --j*)
+      CUSTOM_JOBS="${arg#--j}"
+      FLAG_CUSTOM_JOBS=y
+      ;;
+  esac
 done
 
 clone_ak3() {
-    rm -rf $AK3_DIR
-    git clone --depth=1 https://github.com/GustavoMends/AnyKernel3 -b alioth $AK3_DIR
+  rm -rf $AK3_DIR
+  git clone --depth=1 https://github.com/GustavoMends/AnyKernel3 -b alioth $AK3_DIR
 }
 
 push_msg() {
-    local message="$1"
-    curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
-         -d "chat_id=$CHAT_ID&parse_mode=html&text=$message" \
-         -d "reply_to_message_id=$TOPIC_ID"
+  local message="$1"
+  curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
+    -d "chat_id=$CHAT_ID&parse_mode=html&text=$message" \
+    -d "reply_to_message_id=$TOPIC_ID"
 }
 
 push() {
-    curl -F chat_id="$CHAT_ID" -F reply_to_message_id="$TOPIC_ID" -F document=@"$1" \
-       "https://api.telegram.org/bot$BOT_TOKEN/sendDocument"
+  curl -F chat_id="$CHAT_ID" -F reply_to_message_id="$TOPIC_ID" -F document=@"$1" \
+    "https://api.telegram.org/bot$BOT_TOKEN/sendDocument"
 
 }
 
 build_kramel() {
-START=$(date +"%s")
+  START=$(date +"%s")
 
-    TC_NAME=$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-    push_msg "Build started"
+  TC_NAME=$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+  push_msg "Build started"
 
-    if [ "${FLAG_FULL_JOBS}" = 'y' ]; then
-        JOBS="$ALL_PROCS"
-    elif [ "${FLAG_CUSTOM_JOBS}" = 'y' ]; then
-        JOBS="$CUSTOM_JOBS"
-    else
-        JOBS=12
-    fi
+  if [ "${FLAG_FULL_JOBS}" = 'y' ]; then
+    JOBS="$ALL_PROCS"
+  elif [ "${FLAG_CUSTOM_JOBS}" = 'y' ]; then
+    JOBS="$CUSTOM_JOBS"
+  else
+    JOBS=12
+  fi
 
-    export ARCH=arm64
-    export SUBARCH=arm64
+  export ARCH=arm64
+  export SUBARCH=arm64
 
-    make O=out CC=clang $DEFCONFIG
-    PATH="$TC_DIR/bin:${PATH}" make -j$JOBS O=out CC="clang" CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi-  LLVM=1 LLVM_IAS=1 &> build_log.txt
+  make O=out CC=clang $DEFCONFIG
+  PATH="$TC_DIR/bin:${PATH}" make -j$JOBS O=out CC="clang" CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 &>build_log.txt
 
-	if ! [ -a "$DTB_IMG" ] && [ -a "$DTBO" ] && [ -a "$IMAGE" ];
-	   then
-	       push "build_log.txt" && push_msg "BUild Failed!"
-	       exit 1
-	fi
-    clone_ak3
+  if ! [ -a "$DTB_IMG" ] && [ -a "$DTBO" ] && [ -a "$IMAGE" ]; then
+    push "build_log.txt" && push_msg "BUild Failed!"
+    exit 1
+  fi
+  clone_ak3
 }
 
 function zipping() {
 
-    mv $IMAGE $AK3_DIR
-    mv $DTBO $AK3_DIR
-    mv $DTB_IMG $DTB
-    mv $DTB $AK3_DIR
-    cd $AK3_DIR
+  mv $IMAGE $AK3_DIR
+  mv $DTBO $AK3_DIR
+  mv $DTB_IMG $DTB
+  mv $DTB $AK3_DIR
+  cd $AK3_DIR
 
-    zip -r9 ${KERNEL_NAME} *
-    push_msg "Build completed successfully%0ATotal time elapsed: <b> $(($DIFF / 60))min $(($DIFF % 60))s </b>"
-    push "$KERNEL_NAME"
+  zip -r9 ${KERNEL_NAME} *
+  push_msg "Build completed successfully%0ATotal time elapsed: <b> $(($DIFF / 60))min $(($DIFF % 60))s </b>"
+  push "$KERNEL_NAME"
 }
 
 build_kramel
