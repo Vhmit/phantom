16240406026b6436e81fb4525cb3b3e0ff65bf70
Gustavo Mendes <gusttavo.me@outlook.com>
2025-01-22 20:37:43 +0000

    kramel: Rewrite from scratch
    Change-Id: I8e859418e6763ec71302716b99ce505c8e80c5a3
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- kramel.sh
+++ kramel.sh
@@ -1,125 +1,140 @@
 #!/bin/bash
 
-KERNEL_DIR="$(pwd)"
-
 # User vars
 export KBUILD_BUILD_HOST="ArchLinux"
 export KBUILD_BUILD_USER="GustavoMends"
 
-# Device vars
-DEVICE="alioth"
-DEFCONFIG="vendor/alioth_defconfig"
-
-# DIRs
-AK3_DIR=$KERNEL_DIR/AnyKernel3
-OUTPUT_DIR="$KERNEL_DIR/out/arch/arm64/boot"
+# Dir
+KERNEL_DIR=$(pwd)
+OUT_DIR="out"
 TC_DIR="$KERNEL_DIR/clang"
 
-# TimeZone
-DATE=$(TZ=America/Sao_Paulo date +"%Y%m%d-%T")
-TM=$(date +"%F%S")
+# Date
+TM=$(date '+%Y%m%d-%H%M')
 
-# Kernel Version
-KERNEL_NAME=N0KRAMEL-$DEVICE-${TM}.zip
+# Kramel name
+if [ "${BUILD_TYPE}" = 'REL' ]; then
+    ZIP_NAME="N0kramel-alioth-$TM"
+else
+    COMMIT_SHA="$(git rev-parse --short HEAD)"
+    ZIP_NAME="N0kramel-alioth-$TM-$COMMIT_SHA"
+fi
 
-# Latest commit
-COMMIT_HEAD=$(git log --oneline -1)
+# Kramel Version
+KERVER=$(make kernelversion)
 
-# Files
-DTB=$OUTPUT_DIR/dtb
-DTBO=$OUTPUT_DIR/dtbo.img
-DTB_IMG=$OUTPUT_DIR/dtb.img
-IMAGE=$OUTPUT_DIR/Image
+clone_tc() {
+    mkdir -p clang && cd clang
 
-# Arg parsing
-for arg in "$@"; do
-  case "$arg" in
-    --neutron-clang)
-      mkdir clang && cd clang
-      bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) -S
-      cd ..
-      ;;
-    --zyc-clang)
-      mkdir clang && cd clang
-      ZYC_VERSION=18.0.0
-      wget https://raw.githubusercontent.com/ZyCromerZ/Clang/main/Clang-main-lastbuild.txt && V="$(cat Clang-main-lastbuild.txt)"
-      wget -q https://github.com/ZyCromerZ/Clang/releases/download/${ZYC_VERSION}-$V-release/Clang-${ZYC_VERSION}-$V.tar.gz && tar -xf Clang-${ZYC_VERSION}-$V.tar.gz
-      cd ..
-      ;;
-    --aosp-clang)
-      AOSP_REVISION=r536225
-      git clone --depth=1 https://gitlab.com/kei-space/clang/$AOSP_REVISION.git clang
-      ;;
-    --full-jobs)
-      ALL_PROCS=$(nproc)
-      FLAG_FULL_JOBS=y
-      ;;
-    --j*)
-      CUSTOM_JOBS="${arg#--j}"
-      FLAG_CUSTOM_JOBS=y
-      ;;
-  esac
-done
+    case "$TC_TYPE" in
+        AOSP)
+            wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r547379.tgz
+            tar -xf clang-r547379.tgz
+            ;;
+        Neutron)
+            bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) -S
+            ;;
+        ZYC)
+            wget https://raw.githubusercontent.com/ZyCromerZ/Clang/main/Clang-main-lastbuild.txt
+            V="$(cat Clang-main-lastbuild.txt)"
+            wget -q https://github.com/ZyCromerZ/Clang/releases/download/18.0.0-$V-release/Clang-18.0.0-$V.tar.gz
+            tar -xf Clang-18.0.0-$V.tar.gz
+            ;;
+    esac
 
-clone_ak3() {
-  rm -rf $AK3_DIR
-  git clone --depth=1 https://github.com/GustavoMends/AnyKernel3 -b alioth $AK3_DIR
+    cd ..
 }
 
-push_msg() {
-  local message="$1"
-  curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
-    -d "chat_id=$CHAT_ID&parse_mode=html&text=$message" \
-    -d "reply_to_message_id=$TOPIC_ID"
+build_kernel() {
+    export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+    export COMMIT_HEAD=$(git log --oneline -1)
+
+    export ARCH=arm64
+    export SUBARCH=arm64
+    export PATH="$TC_DIR/bin:$PATH"
+
+    START_TIME=$(date +%s)
+    send_msg "<b>CI Build Triggered</b>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0A<b>Top Commit : </b><code>$COMMIT_HEAD</code>"
+    make O=$OUT_DIR CC=clang vendor/alioth_defconfig &> defc_log.txt
+    if [ $? -ne 0 ]; then
+        send_msg "Build Failed!"
+        send_file defc_log.txt
+        exit 1
+    fi
+
+    make -j12 O=$OUT_DIR CC="clang" CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 &> build_log.txt
+    if [ $? -ne 0 ]; then
+        send_msg "Build Failed!"
+        send_file build_log.txt
+        exit 1
+    fi
+
+    END_TIME=$(date +%s)
+    BUILD_TIME=$(elapsed_time)
+
+    send_msg "Build completed successfully%0ATotal time elapsed: <b>$BUILD_TIME</b>"
+
+    git clone --depth=1 https://github.com/GustavoMends/AnyKernel3.git -b alioth ak3
 }
 
-push() {
-  curl -F chat_id="$CHAT_ID" -F reply_to_message_id="$TOPIC_ID" -F document=@"$1" \
-    "https://api.telegram.org/bot$BOT_TOKEN/sendDocument"
+move_files() {
+    if ! [ -a "out/arch/arm64/boot/Image" ]; then
+        send_msg "Kernel image not found!"
+        exit 1
+    fi
 
+    mv $OUT_DIR/arch/arm64/boot/dtb.img $OUT_DIR/arch/arm64/boot/dtb
+    mv $OUT_DIR/arch/arm64/boot/dtb ak3/
+    mv $OUT_DIR/arch/arm64/boot/dtbo.img ak3/
+    mv $OUT_DIR/arch/arm64/boot/Image ak3/
 }
 
-build_kramel() {
-  START=$(date +"%s")
-
-  TC_NAME=$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-  push_msg "Build started"
-
-  if [ "${FLAG_FULL_JOBS}" = 'y' ]; then
-    JOBS="$ALL_PROCS"
-  elif [ "${FLAG_CUSTOM_JOBS}" = 'y' ]; then
-    JOBS="$CUSTOM_JOBS"
-  else
-    JOBS=12
-  fi
-
-  export ARCH=arm64
-  export SUBARCH=arm64
-
-  make O=out CC=clang $DEFCONFIG
-  PATH="$TC_DIR/bin:${PATH}" make -j$JOBS O=out CC="clang" CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 &>build_log.txt
-
-  if ! [ -a "$DTB_IMG" ] && [ -a "$DTBO" ] && [ -a "$IMAGE" ]; then
-    push "build_log.txt" && push_msg "BUild Failed!"
-    exit 1
-  fi
-  clone_ak3
+zipping() {
+    cd ak3
+    zip -r9 $ZIP_NAME *
+    if [ $? -eq 0 ]; then
+        send_file $ZIP_NAME.zip
+    else
+        send_msg "Failed to zip file!"
+        exit 1
+    fi
 }
 
-function zipping() {
-
-  mv $IMAGE $AK3_DIR
-  mv $DTBO $AK3_DIR
-  mv $DTB_IMG $DTB
-  mv $DTB $AK3_DIR
-  cd $AK3_DIR
-
-  zip -r9 ${KERNEL_NAME} *
-  push_msg "Build completed successfully%0ATotal time elapsed: <b> $(($DIFF / 60))min $(($DIFF % 60))s </b>"
-  push "$KERNEL_NAME"
+send_msg() {
+    local MSG="$1"
+    curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
+         -d "chat_id=$CHAT_ID&parse_mode=html&text=$MSG"
 }
 
-build_kramel
-END=$(date +"%s")
-DIFF=$(($END - $START))
+send_file() {
+    local FILE="$1"
+    curl -F chat_id="$CHAT_ID" -F document=@"$FILE" \
+       "https://api.telegram.org/bot$BOT_TOKEN/sendDocument"
+}
+
+elapsed_time() {
+    local ELAPSED=$((END_TIME - START_TIME))
+
+    local HOURS=$((ELAPSED / 3600))
+    local MINUTES=$(((ELAPSED % 3600) / 60))
+    local SECONDS=$((ELAPSED % 60))
+
+    if [ $HOURS -eq 0 ]; then
+        if [ $SECONDS -le 9 ]; then
+            echo "${MINUTES}min"
+        else
+            echo "${MINUTES}min ${SECONDS}s"
+        fi
+    else
+        if [ $SECONDS -le 9 ]; then
+            echo "${HOURS}h ${MINUTES}min"
+        else
+            echo "${HOURS}h ${MINUTES}min ${SECONDS}s"
+        fi
+    fi
+}
+
+clone_tc
+build_kernel
+move_files
 zipping
