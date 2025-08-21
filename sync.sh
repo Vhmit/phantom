5d382a7cd637d4a8237e143d8a45ba969447fd86
Vhmit <viktor.9630@protonmail.com>
2025-08-21 09:29:49 -0300

    phantom: sync: Add msg for inicied
    Change-Id: I4dab72a6b87a19db675c2cc9484bd5c2ce03d691

--- sync.sh
+++ sync.sh
@@ -14,6 +14,8 @@ send_msg() {
     -d disable_web_page_preview=true
 }
 
+send_msg "✈️ Sync inicied!"
+
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 
 if [ $? -ne 0 ]; then
